08db90ad7aeb56427dfca446eecbf06325b0311b
juhwan kim <jwhksl.kim@samsung.com>
2022-11-20 20:30:06 +0900

    add tmap and edit details

--- src/Components/Location.js
+++ src/Components/Location.js
@@ -11,7 +11,7 @@ const FlexColumn = styled.div`
 
 const FlexRow = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   padding-top: 20px;
   padding-bottom: 20px;
 `;
@@ -26,15 +26,10 @@ const AdressFont = styled.span`
   padding-bottom: 20px;
 `;
 
-const StylesImage = styled.img`
-  padding-right: ${(props) => props.isLeft && "40px"};
-  padding-left: ${(props) => !props.isLeft && "40px"};
-`;
-
 const TextBlock = styled.div`
-  width: 380px;
+  width: 340px;
   font-size: 15px;
-  margin-top: 30px;
+  margin-top: ${(props) => props.top || '30px'};
 `;
 
 const Bold = styled.span`
@@ -58,31 +53,29 @@ function HowToGo() {
 function HowToGoDetails() {
   return (
     <TextBlock>
-      <Bold>JW 메리어트 서울</Bold> TEL 02-6282-6262{" "}
+      <Bold>호텔 전화번호</Bold><br />
+      JW 메리어트 서울 | TEL 02-6282-6262{" "}
       <a href={`tel:0262826262`}>
         <img src="/wedding/call.png" alt="call" height={"15px"} />
       </a>
       <br />
       <br />
-      <Bold>지하철 [3,7,9호선 고속터미널역] </Bold> <br />
-      3,7,9호선 환승역사 7번 출구 방향 에스컬레이터 이용 <br />
-      <Bold>→</Bold> 호남선 방면 에스컬레이터 이용 <br />
-      <Bold>→</Bold> 신세계백화점 강남점 2층에서 BVLGARI(불가리) 오른편 아케이드{" "}
+      <Bold>지하철 안내</Bold><br />
+      3,7,9호선 환승역사 7번 출구 방향 에스컬레이터<br />
+      <Bold>→</Bold> 호남선 방면 에스컬레이터 이용<br />
+      <Bold>→</Bold> 신세계백화점 2층에서 BVLGARI(불가리) 오른편<br />
+      <Bold>→</Bold> JW 메리어트 호텔 로비로 연결<br />
       <br />
-      <Bold>→</Bold> 호텔 로비 또는 3, 7, 9호선 3호선 출구에서 왼편 10m <br />
-      <Bold>→</Bold> 호텔 정문 <br />
-      <br />
-      <br />
-      <Bold>주차</Bold> <br />
+      <Bold>주차 안내</Bold> <br />
       호텔, 신세계 백화점 주차장 6시간 무료 주차 가능 <br />
-      <Warn>(파미에스테이션 주차장은 유료)</Warn> <br />
-      주차가 매우 혼잡할 수 있사오니 가급적 대중교통 이용을 부탁드립니다.
+      <Warn>※ 파미에스테이션 주차장은 유료</Warn> <br />
+      ※ 주차가 매우 혼잡할 수 있습니다.
     </TextBlock>
   );
 }
 const KakaoMap = styled.div`
   max-width: 780px;
-  width: 380px;
+  width: 340px;
   min-width: 200px;
   height: 250px;
   display: block;
@@ -111,25 +104,36 @@ const Location = () => {
         <KakaoMap id="map" />
       </FlexColumn>
       <FlexRow>
-        <a href="https://map.kakao.com/link/to/195802694">
-          <StylesImage
-            src="/wedding/kakao_map.png"
-            alt="kakao"
-            height={"50px"}
-            isLeft
-          />
-        </a>
-        <a
-          href={`nmap://route/car?dlat=37.5041&dlng=127.0048&dname=${encodeURI(
-            "JW 메리어트 호텔 서울"
-          )}&appname=wedding`}
-        >
-          <StylesImage
-            src="/wedding/naver_map.png"
-            alt="naver"
-            height={"50px"}
-          />
-        </a>
+        <TextBlock top ='0px'>
+          <FlexRow>
+            <a href="https://map.kakao.com/link/to/195802694">
+              <img
+                src="/wedding/kakao_map.png"
+                alt="kakao"
+                height={"50px"}
+                isLeft
+              />
+            </a>
+            <a
+              href={`nmap://route/car?dlat=37.5041&dlng=127.0048&dname=${encodeURI(
+                "JW 메리어트 호텔 서울"
+              )}&appname=wedding`}
+            >
+              <img
+                src="/wedding/naver_map.png"
+                alt="naver"
+                height={"50px"}
+              />
+            </a>
+            <a href={`https://apis.openapi.sk.com/tmap/app/routes?appKey=${process.env.REACT_APP_TMAP_KEY}&name=JW메리어트호텔서울&lon=127.004&lat=37.5041`}>
+              <img
+                src = "/wedding/tmap.jpg"
+                alt="tmap"
+                height={"50px"}
+              />
+            </a>
+          </FlexRow>
+        </TextBlock>
       </FlexRow>
       <FlexRow>
         <HowToGoDetails />
